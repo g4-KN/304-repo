4c363752f81af6f456765b8e387279382ca17636
Crispy <crpoon@gmail.com>
2015-03-24 23:58:19 -0700

    Added getting member from database

--- src/com/cpsc304/coffeeshop/service/MemberServiceImpl.java
+++ src/com/cpsc304/coffeeshop/service/MemberServiceImpl.java
@@ -1,10 +1,13 @@
 package com.cpsc304.coffeeshop.service;
 
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import com.cpsc304.coffeeshop.database.LocalConnection;
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
@@ -17,25 +20,25 @@ public class MemberServiceImpl {
 		this.connection = lc.getConnection();
 	}
 
-    public  ObservableList<Map> getMemberById(int memberId) {
-        String query = "select * from Member where MemberId = ?";
-        ObservableList<Map> resultData = FXCollections.observableArrayList();
+    public List<Map<String, String>> getMemberById(int memberId) {
+        String query = "SELECT * FROM member m, postalcodereference p WHERE m.postalcode = p.postalcode AND m.memberId = ?";
+        List<Map<String, String>> resultData = new ArrayList<Map<String, String>>();
         try {
             PreparedStatement stmt = connection.prepareStatement(query);
             stmt.setInt(1, memberId);
             ResultSet rs = stmt.executeQuery();
 
             while (rs.next()){
-                //| MemberId | PointBalance | Name  | Phone    | HouseNo | Street     | PostalCode |
-                //System.out.println(rs.getString("Name"));
                 Map<String, String> nm = new HashMap<>();
                 nm.put("MemberId", "" + rs.getInt("MemberId"));
                 nm.put("PointBalance", "" + rs.getInt("PointBalance"));
                 nm.put("Name", rs.getString("Name"));
-                nm.put("Phone", "" + rs.getInt("Phone"));
+                nm.put("Phone", "" + rs.getString("Phone"));
                 nm.put("HouseNo", "" + rs.getInt("HouseNo"));
                 nm.put("Street", rs.getString("Street"));
                 nm.put("PostalCode", rs.getString("PostalCode"));
+                nm.put("City", rs.getString("City"));
+                nm.put("Province", rs.getString("Province"));
                 resultData.add(nm);
             }
 
