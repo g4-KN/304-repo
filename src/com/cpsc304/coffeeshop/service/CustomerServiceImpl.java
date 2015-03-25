add8aa137752d3e5f2038d6f5efabcf6dcfee1f3
ken ng <kenng@kens-MacBook-Air.local>
2015-03-25 08:18:29 -0700

    finished off the customer query

--- src/com/cpsc304/coffeeshop/service/CustomerServiceImpl.java
+++ src/com/cpsc304/coffeeshop/service/CustomerServiceImpl.java
@@ -1,9 +1,10 @@
 package com.cpsc304.coffeeshop.service;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import com.cpsc304.coffeeshop.database.LocalConnection;
 
@@ -16,18 +17,162 @@ public class CustomerServiceImpl {
 		this.connection = lc.getConnection();
 	}
 	
-	public void testQuery(int customerId) {
-		try {
-			String query = "SELECT * FROM customer c";
-			PreparedStatement ps = connection.prepareStatement("SELECT * FROM customer c WHERE c.id = ?");
-			ps.setInt(1, 123);
-			ResultSet rs = ps.executeQuery();
-			while (rs.next()) {
-				System.out.println(rs.getInt(1));
-			}
-			ps.close();
-		} catch (SQLException sqle) {
-			sqle.printStackTrace();
-		}
-	}
+	 public List<Map<String, String>> showStoreInfo() throws SQLException {
+	        String query = "SELECT * FROM store";
+	        List<Map<String, String>> resultData = new ArrayList<Map<String, String>>();
+	        PreparedStatement stmt = null;
+	        try {
+	            stmt = connection.prepareStatement(query);
+	            ResultSet rs = stmt.executeQuery();
+
+	            while (rs.next()){
+	                Map<String, String> nm = new HashMap<>();
+	                nm.put("StoreId", "" + rs.getInt("StoreId"));
+	                nm.put("HouseNo", "" + rs.getInt("HouseNo"));
+	                nm.put("Street", rs.getString("Street"));
+	                nm.put("PostalCode", "" + rs.getString("PostalCode"));
+	               
+	                resultData.add(nm);
+	                
+	              //  System.out.println(nm);
+	            }
+
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	        } finally {
+	            if (stmt != null) {
+	                stmt.close();
+	            }
+	        }
+	        return resultData;
+	  }
+	 
+	 public List<Map<String, String>> showFoodInfo() throws SQLException {
+	        String query = "SELECT * FROM food";
+	        List<Map<String, String>> resultData = new ArrayList<Map<String, String>>();
+	        PreparedStatement stmt = null;
+	        try {
+	            stmt = connection.prepareStatement(query);
+	            ResultSet rs = stmt.executeQuery();
+
+	            while (rs.next()){
+	                Map<String, String> nm = new HashMap<>();
+	                nm.put("pointReward", "" + rs.getInt("pointReward"));
+	                nm.put("pointCost", "" + rs.getInt("pointCost"));
+	                nm.put("price", "" + rs.getInt("price"));
+	                nm.put("foodName", "" + rs.getString("foodName"));
+	               
+	                resultData.add(nm);
+	                
+	               // System.out.println(nm);
+	            }
+
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	        } finally {
+	            if (stmt != null) {
+	                stmt.close();
+	            }
+	        }
+	        return resultData;
+	 }
+	 
+	 public List<Map<String, String>> showDrinkInfo() throws SQLException {
+	        String query = "SELECT * FROM drink";
+	        List<Map<String, String>> resultData = new ArrayList<Map<String, String>>();
+	        PreparedStatement stmt = null;
+	        try {
+	            stmt = connection.prepareStatement(query);
+	            ResultSet rs = stmt.executeQuery();
+
+	            while (rs.next()){
+	                Map<String, String> nm = new HashMap<>();
+	                nm.put("pointReward", "" + rs.getInt("pointReward"));
+	                nm.put("pointCost", "" + rs.getInt("pointCost"));
+	                nm.put("price", "" + rs.getInt("price"));
+	                nm.put("drinkName", "" + rs.getString("drinkName"));
+	                nm.put("size", "" + rs.getString("size"));
+	               
+	                resultData.add(nm);
+	                
+	              //  System.out.println(nm);
+	            }
+
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	        } finally {
+	            if (stmt != null) {
+	                stmt.close();
+	            }
+	        }
+	        return resultData;
+	 }
+	 
+	 public List<Map<String, String>> showDrinkMenuByStoreId(int storeId) throws SQLException {
+		 // joining tables together
+	        String query = "SELECT * FROM drink d, drinkMenu dm WHERE dm.drinkName = d.drinkName AND dm.storeId = ?";
+	        List<Map<String, String>> resultData = new ArrayList<Map<String, String>>();
+	        PreparedStatement stmt = null;
+	        try {
+	            stmt = connection.prepareStatement(query);
+	            stmt.setInt(1, storeId);
+	            ResultSet rs = stmt.executeQuery();
+	            
+	            while (rs.next()){
+	            	// this result adding process act as a second layer of filtering
+	                Map<String, String> nm = new HashMap<>();
+	                nm.put("pointReward", "" + rs.getInt("pointReward"));
+	                nm.put("pointCost", "" + rs.getInt("pointCost"));
+	                nm.put("price", "" + rs.getInt("price"));
+	                nm.put("drinkName", "" + rs.getString("drinkName"));
+	                nm.put("size", "" + rs.getString("size"));
+	               
+	                resultData.add(nm);
+	                
+	               // System.out.println(nm);
+	            }
+
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	        } finally {
+	            if (stmt != null) {
+	                stmt.close();
+	            }
+	        }
+	        return resultData;
+	 }
+	 public List<Map<String, String>> showFoodMenuByStoreId(int storeId) throws SQLException {
+		 // joining tables together
+	        String query = "SELECT * FROM food f, foodMenu fm WHERE fm.foodName = f.foodName AND fm.storeId = ?";
+	        List<Map<String, String>> resultData = new ArrayList<Map<String, String>>();
+	        PreparedStatement stmt = null;
+	        try {
+	            stmt = connection.prepareStatement(query);
+	            stmt.setInt(1, storeId);
+	            ResultSet rs = stmt.executeQuery();
+	            
+	            while (rs.next()){
+	            	// this result adding process act as a second layer of filtering
+	                Map<String, String> nm = new HashMap<>();
+	                nm.put("pointReward", "" + rs.getInt("pointReward"));
+	                nm.put("pointCost", "" + rs.getInt("pointCost"));
+	                nm.put("price", "" + rs.getInt("price"));
+	                nm.put("foodName", "" + rs.getString("foodName"));
+	               
+	                resultData.add(nm);
+	                
+	               // System.out.println(nm);
+	            }
+
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	        } finally {
+	            if (stmt != null) {
+	                stmt.close();
+	            }
+	        }
+	        return resultData;
+	 }
+	  
+	  
 }
